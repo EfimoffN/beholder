e78901f497c5111f7d16af71790179d633928718
nefimov <nefimov@mobiledep.ru>
2024-01-30 22:11:39 +0300

    added config

--- types/configApp.go
+++ types/configApp.go
@@ -1,44 +1,27 @@
 package types
 
 type ConfigApp struct {
-	SessionTG string
-	ConfigDB  ConfigPsg
-	ConfigKfk string
-}
-
-type ConfigPsg struct {
-	Host     string
-	User     string
-	Password string
-	DBname   string
-	SSLmode  string
-	Port     string
-}
-
-type SessionRow struct {
-	SessionID   string `db:"sessionid"`
-	AppID       int    `db:"appid"`
-	AppHash     string `db:"apphash"`
-	PhoneNumber string `db:"phonenumber"`
-	Session     string `db:"sessiontxt"`
-}
-
-type LastMsgRow struct {
-	MsgID      string `db:"msgid"`
-	ChanneTgID int64  `db:"channelidtg"`
-	LastMsgID  int    `db:"lastidmsg"`
-	SessionID  string `db:"sessionid"`
-}
-
-type ChannelRow struct {
-	ChannelID    string `db:"channelid"`
-	ChannelTgID  int64  `db:"channelidtg"`
-	ChannelName  string `db:"channelname"`
-	ChannelLink  string `db:"channellink"`
-	ChannelClose bool   `db:"channelclose"`
+	BeholderTG SessionTG
+	ConfigKfk  ProducerKfk
 }
 
 type ReadyRepost struct {
 	ChannelTgID int64  `json:"ChannelTgID"`
 	MessageLink string `json:"MessageLink"`
+}
+
+type SessionTG struct {
+	Port          string
+	SessionTG     string
+	PhoneNumber   string
+	AppID         int
+	AppHASH       string
+	SessionOptMin int // minimum value of random delay in milliseconds
+	SessionOptMax int // maximum value of random delay in milliseconds
+}
+
+type ProducerKfk struct {
+	ProducerGroup  string
+	ProducerTopic  string
+	ProducerBroker string
 }
