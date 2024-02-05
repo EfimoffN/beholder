accf28af1cef877730e8f186c07557cafde532ea
nefimov <nefimov@mobiledep.ru>
2024-02-05 19:03:25 +0300

    added search channel

--- tg_beholder/beholder.go
+++ tg_beholder/beholder.go
@@ -7,6 +7,7 @@ import (
 	"github.com/EfimoffN/beholder/types"
 	"github.com/gotd/td/telegram/message"
 	"github.com/gotd/td/telegram/updates"
+	"github.com/pkg/errors"
 
 	"github.com/gotd/td/tg"
 	"go.uber.org/zap"
@@ -45,10 +46,21 @@ func (tgb *TgBeholder) CheckedPosts() error {
 		if !ok {
 			return nil
 		}
+		// tgId := pub.FromID
+		// channel, err := tgb.SerchChannelByID(0)
+		// if err != nil {
+		// 	return err
+		// }
+
+		ch, ok := pub.PeerID.(*tg.PeerChannel)
+		if !ok {
+			return nil
+		}
+
 		// добавить поиск каналов по id
 		acceptedPublication := types.AcceptedPublication{
-			// ChannelTgID: update.Message,
-			ChannelTgID: int64(message.GetID()),
+			MessageID:   int64(pub.ID),
+			ChannelTgID: ch.ChannelID,
 			MessageLink: "https://t.me/" + pub.PostAuthor + "/" + strconv.Itoa(pub.ID),
 		}
 
@@ -113,4 +125,32 @@ func (tgb *TgBeholder) CheckedPosts() error {
 	// https://github.com/gotd/td/blob/main/examples/updates/main.go
 
 	return nil
+}
+
+func (tgb *TgBeholder) SerchChannelByID(channelID int64) (*tg.Channel, error) {
+	api := tgb.client.API()
+
+	req := []tg.InputChannelClass{
+		&tg.InputChannel{
+			ChannelID: channelID,
+		},
+	}
+
+	channelList, err := api.ChannelsGetChannels(tgb.ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	if channelList.Zero() {
+		err := errors.New("channel not found")
+
+		return nil, err
+	}
+
+	channelData, ok := channelList.GetChats()[0].(*tg.Channel)
+	if !ok {
+		return nil, errors.New("can not convert to channel")
+	}
+
+	return channelData, nil
 }
